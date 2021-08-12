3786dfadfb572845d805a10660d125a58c107ec1
minhtranjh <minh.tran@journeyh.io>
2021-08-12 11:26:24 +0700

    Revert "Revert "Add "capacity" property to listing""
    This reverts commit 7a42b013d4f6831586df74b0e26fb84d78f09ae2.

--- src/components/EditListingPricingPanel/EditListingPricingPanel.js
+++ src/components/EditListingPricingPanel/EditListingPricingPanel.js
@@ -30,8 +30,12 @@ const EditListingPricingPanel = props => {
 
   const classes = classNames(rootClassName || css.root, className);
   const currentListing = ensureOwnListing(listing);
-  const { price } = currentListing.attributes;
+  const { price , cleaningFee } = currentListing.attributes;
+  const cleaningFeeAsMoney = cleaningFee
+    ? new Money(cleaningFee.amount, cleaningFee.currency)
+    : null;
 
+  const initialValues = { price, cleaningFee: cleaningFeeAsMoney };
   const isPublished = currentListing.id && currentListing.attributes.state !== LISTING_STATE_DRAFT;
   const panelTitle = isPublished ? (
     <FormattedMessage
@@ -46,8 +50,17 @@ const EditListingPricingPanel = props => {
   const form = priceCurrencyValid ? (
     <EditListingPricingForm
       className={css.form}
-      initialValues={{ price }}
-      onSubmit={onSubmit}
+      initialValues={initialValues}
+      onSubmit={values => {
+        const { price, cleaningFee = null } = values;
+        const updatedValues = {
+          price,
+          publicData: {
+            cleaningFee: { amount: cleaningFee.amount, currency: cleaningFee.currency },
+          },
+        };
+        onSubmit(updatedValues);
+      }}
       onChange={onChange}
       saveActionMsg={submitButtonText}
       disabled={disabled}
